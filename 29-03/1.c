01daeb150fc2948293f5acd954021e2c7536cc9e
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2016-03-28 23:47:16 -0300

    Added files via upload

---
+++ 29-03/1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int mult(int a, int b){
+
+    int vezes = 0;
+
+    while(a > 1){
+        a = a/2;
+        vezes++;
+    }
+
+
+    int elevado = pow(2,vezes);
+    return (b + b*(elevado));
+}
+
+int main(void){
+    int n;
+    printf("Digite quantas vezes deseja fazer a operação:");
+    scanf("%d",&n);
+    while(n<=0){
+        printf("Digite um numero acima de \n");
+     }
+    while(n>0){
+        int x,y;
+        printf("Digite o primeiro numero:");
+        scanf("%d",&x);
+        printf("Digite o segundo numero:");
+        scanf("%d",&y);
+        printf("Produto dos numeros %i e %i(segundo a questao): %i\n",x,y,mult(x, y));
+        n--;
+    }
+return 0;
+}
