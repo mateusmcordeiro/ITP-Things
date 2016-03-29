751f46b42781cd7182ae2573704eaec36bb7869c
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2016-03-29 09:57:51 -0300

    Update 2.c

--- 29-03/2.c
+++ 29-03/2.c
@@ -3,8 +3,12 @@
 #include <math.h>
 
 
-int mdc(int maior , int num[] ){
-    
+int mdc(int maior , int num[] , int n){
+    int resto[n],i;
+    for(i=0;i<n-1;i++){
+            resto[i] = num[i]%maior;
+    }
+
 }
 
 int bigger(int num[],int n){
@@ -17,10 +21,26 @@ int bigger(int num[],int n){
     }
  return maior;
 }
+
+int tiraMaior(int num[],int n,int maior){
+    int newNum[n-1];
+    bool achou = false;
+    for(i=0;i<n;i++){
+        if ((maior != num[i])&&(achou == false)){
+            newNum[i] = num[i];
+            achou = false;
+        }else{
+            newNum[i-1] = num[i];
+            achou = true;
+        }
+    }
+    return newNum;
+}
+
 int dist(int n){
     int i,num[n];
     for(i=0;i<n;i++){
-        printf("Digite o %i numero",i+1);
+        printf("Digite o %iº numero",i+1);
         scanf("%i",&num[i]);
     }
  return num;
@@ -28,10 +48,12 @@ int dist(int n){
 
 int main(void){
     int n;
-    printf("quantos numeros voce vai usar no mdc?");
+    printf("quantos numeros você vai usar no mdc?");
     scanf("%i",&n);
     int num = dist(n);
     int maior = bigger(num,n);
+    int newNum = tiraMaior(num,n,maior);
+    int mdc = mdc(maior,newNum,n);
     printf("maior = %d",maior);
 return 0;
 
