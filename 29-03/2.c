f5f3a9ed0bead416469d84afb61614c9a452f091
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2016-03-29 08:02:32 -0300

    Update 2.c

--- 29-03/2.c
+++ 29-03/2.c
@@ -1,1 +1,71 @@
+#include <stdio.h>
 
+#include <math.h>
+
+
+int mdc(int a, int b){
+
+
+    
+ int n;
+ scanf("%i",&n);
+int num[n];
+int r,i,j,maior;
+for(i=0;i<n;i++){
+	for (j=0;j<n;j++){
+		
+		if ((num[j]>num[i])){
+			
+		maior=num[j];
+		
+		}else{
+			maior = num[i];
+		}
+	}
+}
+
+for(i=0;i<n;i++){
+	for (j=0;j<n;j++){
+		a
+		
+	}
+}
+
+}
+
+
+int main(void){
+
+    int n;
+
+    printf("Digite quantas vezes deseja fazer a operação:");
+
+    scanf("%d",&n);
+
+    while(n<=0){
+
+        printf("Digite um numero acima de \n");
+
+     }
+
+    while(n>0){
+
+        int x,y;
+
+        printf("Digite o primeiro numero:");
+
+        scanf("%d",&x);
+
+        printf("Digite o segundo numero:");
+
+        scanf("%d",&y);
+
+        printf("Produto dos numeros %i e %i(segundo a questao): %i\n",x,y,mult(x, y));
+
+        n--;
+
+    }
+
+return 0;
+
+}
