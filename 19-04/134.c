9c94dc678df4fb1a330a52d9b3095c3d326973b4
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2016-04-18 22:33:05 -0300

    Update 134.c

--- 19-04/134.c
+++ 19-04/134.c
@@ -2,13 +2,12 @@
 #define MAX 100
 
 int main(void){
-
-char cadeia[MAX+1];
-int index;
-fgets(cadeia, sizeof(cadeia),stdin);
-char c = 'f';
-char *ptr = strchr(cadeia,c);
-index=ptr-cadeia;
-printf("Primeira ocorrencia de '%c' esta na posicao %i",c,index);
+  char cadeia[MAX+1];
+  int index;
+  fgets(cadeia, sizeof(cadeia),stdin);
+  char c = 'f';
+  char *ptr = strchr(cadeia,c);
+  index=ptr-cadeia;
+  printf("Primeira ocorrencia de '%c' esta na posicao %i",c,index);
 
 }
