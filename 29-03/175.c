5beb0ca4da9d631ae5e4c344ed47a9f568c020c7
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2016-10-20 18:46:25 -0300

    Indentation code fixes

--- 29-03/175.c
+++ 29-03/175.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 
-int soma_primo(int primos[],int isP[],int n){
+int soma_primo(int primos[],int isP[],int n) {
     int i;
     int soma = 0 ;
-    for(i=0;i<n;i++){
-        if(isP[i] == 1){
+    for(i=0; i<n; i++) {
+        if(isP[i] == 1) {
             soma = soma +primos[i];
         }
     }
     return soma;
 }
-int verifica_primo(int n,int p[]){
+int verifica_primo(int n,int p[]) {
     int isPrimo[n];
     int i,j;
-    for(i=0;i<n;i++){
+    for(i=0; i<n; i++) {
         isPrimo[i] = 1;
     }
-    for(i=0;i<n;i++){
+    for(i=0; i<n; i++) {
         double num = sqrt(p[i]);
         int x  = ceil(num);
-        for(j=2;j<=x;j++){
-            if(p[i]%j == 0){
+        for(j=2; j<=x; j++) {
+            if(p[i]%j == 0) {
                 isPrimo[i] = 0;
             }
         }
@@ -29,9 +29,9 @@ int verifica_primo(int n,int p[]){
     return isPrimo;
 }
 
-int preenche(int n){
+int preenche(int n) {
     int primos[n],i;
-    for(i=0;i<n;i++){
+    for(i=0; i<n; i++) {
         printf("Digite o %i numero\n",i+1);
         scanf("%i",&primos[i]);
     }
@@ -39,13 +39,14 @@ int preenche(int n){
 }
 
 
-int main(){
+int main() {
     int n;
-        printf("Quantos numeros deseja verificar?");
-        scanf("%i",&n);
+    printf("Quantos numeros deseja verificar?");
+    scanf("%i",&n);
     int p = preenche(n);
     int isP = verifica_primo(n,p);
     int s = soma_primo(p,isP,n);
-        printf("soma = %i",s);+--
+    printf("soma = %i",s);
+    +--
     return 0;
 }
